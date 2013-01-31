2f94b3ec0f479bded2e7bf33944c7f5dbdbdad5a
halaszk <halaszk@deathlord.eu>
2013-02-01 00:51:17 +0100

    Ported Zenx CPU governor from AbyssNoteII kernel

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -396,6 +396,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZENX)
+extern struct cpufreq_governor cpufreq_gov_zenx;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_zenx)
 #endif
 
 
